c3f9a78c70b0515db9bafb6880d94a967b7bc542
杨乐 <yangle@ewell.cc>
2023-08-04 20:53:02 +0800

    aa

---
+++ src/main/java/com/example/test/CamundaKeycloakTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaKeycloakTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaKeycloakTestApplication.class, args);
+    }
+
+}
